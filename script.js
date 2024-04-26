a50c22234db6d3c952a3ffa26b7def6763605fb7
Gardiny <igor20091983@gmail.com>
2024-04-26 16:03:14 -0300

    login finalizado

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previne o envio padrão do formulário
+            
+    // Valores esperados
+    var nomeEsperado = "igor";
+    var emailEsperado = "igor@com.br";
+    var senhaEsperada = "admin";
+            
+    // Valores do formulário
+    var nomeDigitado = document.getElementById("nome").value;
+    var emailDigitado = document.getElementById("email").value;
+    var senhaDigitada = document.getElementById("senha").value;
+            
+    // Verifica se os valores digitados correspondem aos esperados
+    if (nomeDigitado === nomeEsperado && emailDigitado === emailEsperado && senhaDigitada === senhaEsperada) {
+        // Redireciona para a página home
+        window.location.href = "igor.html";
+    } else {
+        // Se os valores não coincidirem, exibe uma mensagem de erro
+        alert("Nome, e-mail ou senha incorretos. Por favor, tente novamente.");
+    }
+});
